acd40a51f1208772282eb63b6c4a751c98c8dcd3
Changgil Jeong <ombreman21@gmail.com>
2021-06-23 13:55:47 +0100

    Update Q_2805.py

--- algorithm_week/Q_2805.py
+++ algorithm_week/Q_2805.py
@@ -19,34 +19,34 @@ while start <= end:  # 적절한 벌목 높이를 찾기
 print(end)
 
 ########################################
-# 함수로 풀기
+함수로 풀기
 
-# N, M = map(int, input().split())
-# tree = list(map(int, input().split()))
-#
-#
-# def logSum(height):  # 얻어가는 나무 길이 함수
-#     totalLog = 0
-#     for i in tree:
-#         if i - height > 0:
-#             totalLog += (i - height)
-#
-#     return totalLog
-#
-#
-# def binarySearch(targetHeight):  # 이분탐색 함수
-#     start, end = 0, max(tree)
-#     answer = 0
-#     while start <= end:
-#         midCutting = (start + end) // 2
-#         Sum = logSum(midCutting)
-#         if Sum < targetHeight:
-#             end = midCutting - 1
-#         if Sum >= targetHeight:
-#             answer = midCutting
-#             start = midCutting + 1
-#
-#     return answer
-#
-#
-# print(binarySearch(M))
+N, M = map(int, input().split())
+tree = list(map(int, input().split()))
+
+
+def logSum(height):  # 얻어가는 나무 길이 함수
+    totalLog = 0
+    for i in tree:
+        if i - height > 0:
+            totalLog += (i - height)
+
+    return totalLog
+
+
+def binarySearch(targetHeight):  # 이분탐색 함수
+    start, end = 0, max(tree)
+    answer = 0
+    while start <= end:
+        midCutting = (start + end) // 2
+        Sum = logSum(midCutting)
+        if Sum < targetHeight:
+            end = midCutting - 1
+        if Sum >= targetHeight:
+            answer = midCutting
+            start = midCutting + 1
+
+    return answer
+
+
+print(binarySearch(M))
